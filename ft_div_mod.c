568d80cb2075448d6abd7be664ad8a6660363cc4
Oleksandr Fedota <pm.fedota.a@gmail.com>
2025-08-10 23:24:46 +0200

    ex03 added

---
+++ ft_div_mod.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void ft_div_mod(int a, int b, int *div, int *mod);
+
+int main(void)
+{
+	int a = 12;
+	int b = 5;
+	int *div;
+	int *mod;
+	ft_div_mod(a,b,div,mod);
+	return 0;
+}
+
+
+void ft_div_mod(int a, int b, int *div, int *mod)
+{
+	*div = a / b;
+	*mod = a % b;
+	printf("%d/t%d",*div,*mod);
+}
